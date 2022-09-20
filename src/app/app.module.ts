dfb90902be209f328b27f04f193262a76d7c17ea
AishwaryaJadhav <apjadhav_b20@it.vjti.ac.in>
2022-09-20 12:45:43 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { RepoComponent } from './Components/repo/repo.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import{HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import{ NgxSpinnerModule , NgxSpinnerService} from 'ngx-spinner'
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    RepoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgxSpinnerModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
